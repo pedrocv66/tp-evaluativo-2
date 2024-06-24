172ccf65219b331f3f7bda3a02683b7e853e929f
pedrocv66 <cuevasp@gmail.com>
2024-06-24 10:52:41 -0300

    primeros pasos

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { InicioComponent } from './pages/inicio/inicio.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:"",component:InicioComponent
+  },
+  {
+    path:"",loadChildren:()=>import('./modules/inicio/inicio.module').then(m=>m.InicioModule)
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule], 
+
 })
 export class AppRoutingModule { }
